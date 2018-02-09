d3f68572c4379250153f3240a3e2425d61a09705
Mohsen Azimi <me@azimi.me>
2018-02-08 22:45:37 -0800

    styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     // Add typescript stack into webpack
     `gatsby-plugin-typescript`,
     {
--- src/components/project-tile.tsx
+++ src/components/project-tile.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
+import styled from "styled-components"
 
+import * as colors from '../common/colors'
 
 export interface Project {
     name: string;
@@ -9,32 +11,64 @@ export interface Project {
     website: string;
   }
 
+// prototype: https://codepen.io/mohsen1/pen/bLdNoJ?editors=0110
+const Card = styled.div`
+  border: 1px solid ${colors.bone};
+  /* background: ${colors.grayLight}; */
+  padding: 1rem;
+  margin: 1rem 0;
+  box-shadow: 0 0 1rem ${colors.grayDark};
+
+  * {
+    text-decoration: none;
+  }
+`;
+
+const Title = styled.h3`
+    color: ${colors.mulberry};
+`;
+
+const Description = styled.p`
+    color: ${colors.grayDark};
+`;
+
+
+const Icon = styled.div`
+    display: inline-block;
+    padding: 0.4rem;
+    line-height: 1;
+    border-radius: 5px;
+    font-family: monospace;
+    color: white;
+    background: ${colors.pink};
+    /* background-color: ${(props) => props.color} */
+`;
+
+
+const LanguageIcon: React.StatelessComponent<{language: Project['language']}> = ({ language }) => {
+    switch (language) {
+        case 'JavaScript':
+            return <Icon>JS</Icon>;
+        case 'TypeScript':
+            return <Icon>TS</Icon>;
+        default:
+            return <Icon>{language}</Icon>;
+    }
+}
 
 /** A tile for a open source project */
 export class ProjectTile extends React.Component<Project> {
     public render() {
         const { language, name, description, category, website } = this.props;
         return (
-            <div>
+            <Card>
                 <a href={website} target="_blank">
-                    <h3>{name}</h3>
-                    <p>{description}</p>
+                    <Title>{name}</Title>
+                    <Description>{description}</Description>
                     <p><small>{category}</small></p>
                     <LanguageIcon language={language} />
                 </a>
-            </div>
+            </Card>
         );
-    }
-}
-
-
-const LanguageIcon: React.StatelessComponent<{language: Project['language']}> = ({ language }) => {
-    switch (language) {
-        case 'JavaScript':
-            return <div>JS</div>;
-        case 'TypeScript':
-            return <div>TS</div>;
-        default:
-            return <div>{language}</div>;
     }
 }
