5556602fdd05f8a3a38d945d3c176fb65c5559d2
Mohsen Azimi <me@azimi.me>
2018-02-09 22:39:48 -0800

    nice header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,37 +1,59 @@
 import * as React from 'react';
 import Link from 'gatsby-link';
-import * as styled from 'styled-components';
+import styled from 'styled-components';
 
-import logo from './logo@3x.png';
-
+import * as logo from './logo@3x.png';
 
 import * as colors from '../common/colors';
 
+const HeaderWrapper = styled.div`
+    background: ${colors.charcoal};
+    color: ${colors.white};
+    margin-bottom: '1.45rem';
+    display: flex;
+    justify-content: space-between;
+}
+`;
+
+const LogoLink = styled(Link)`
+    padding: 1rem;
+    display: block;
+`;
+
+const LogoTitle = styled.h1`
+    font-size: 1rem;
+    font-weight: normal;
+    color: ${colors.white};
+    margin: 0;
+`
+const LogoImage = styled.img.attrs({src: logo})`
+    margin: 0 .3rem 0 0;
+    vertical-align: middle;
+    width: 44px;
+`
+
+const RightNav = styled.div`
+    padding: 1rem;
+`
+
+const RightNavLink = styled(Link)`
+    color: ${colors.white};
+    padding: 0 0.5rem;
+    font-family: monospace;
+    text-decoration: none;
+`
+
 export const Header = () => (
-    <div
-        style={{
-            background: colors.purple,
-            marginBottom: '1.45rem',
-        }}
-    >
-        <div
-            style={{
-                margin: '0 auto',
-                maxWidth: 960,
-                padding: '1.45rem 1.0875rem',
-            }}
-        >
-            <h1 style={{ margin: 0 }}>
-                <Link
-                    to="/"
-                    style={{
-                        color: 'white',
-                        textDecoration: 'none',
-                    }}
-                >
-                    Lyft Open Source
-                </Link>
-            </h1>
-        </div>
-    </div>
+    <HeaderWrapper>
+        <LogoLink to='/'>
+            <LogoTitle>
+                <LogoImage />
+                Open Source
+            </LogoTitle>
+        </LogoLink>
+        <RightNav>
+            <RightNavLink to='https://github.com/lyft/'>Github</RightNavLink>
+            <RightNavLink to='https://eng.lyft.com/'>Blog</RightNavLink>
+        </RightNav>
+    </HeaderWrapper>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,6 +43,7 @@ export const pageQuery = graphql`
                     languages
                     categories
                     website
+                    source
                 }
             }
         }
